c400b2e7f3b4e4c2e37109fcbeb0f6f48bc1fd81
sebastian Reyes <sebreyes222gmail.com>
2023-02-10 12:15:00 +0100

    ajout version final form

--- src/components/formulaires/formulaires.js
+++ src/components/formulaires/formulaires.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { useState } from 'react'
 
 const Formulaires = () => {
-  const [first, setFirst] = useState("")
-  const [name, setName] = useState("");
-  const[email, setEmail] = useState("");
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
   const handleSubmit = event => {
     event.preventDefault();
     setError('');
 
-    if (!name || !email || !password || !first) {
+    if (!name || !email || !password) {
       setError('Tous les champs sont requis');
     } else if (!/\S+@\S+\.\S+/.test(email)) {
       setError('L\'email est invalide');
@@ -19,73 +19,44 @@ const Formulaires = () => {
       // Envoyer les données au serveur
     }
   };
+
   return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nom</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+      </div>
 
-<section className="bg-gray-50 dark:bg-gray-900">
-<form class="w-full max-w-sm" onSubmit={handleSubmit}>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        Nom
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"value={first} onChange={event => setFirst(event.target.value)} placeholder='' ></input>
-    </div>
-  </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-        Prénom
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" value={name} onChange={event => setName(event.target.value)} placeholder="prénom"></input>
-    </div>
-  </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-        Email
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="email" value={email} onChange={event => setEmail(event.target.value)} placeholder="email"></input>
-    </div>
-  </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-        mot de passe
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" value={password} onChange={event => setPassword(event.target.value)}placeholder="*********"></input>
-    </div>
-  </div>
-  {error && <p>{error}</p>}
-  
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3"></div>
-    <label class="md:w-2/3 block text-gray-500 font-bold">
-      <input class="mr-2 leading-tight" type="checkbox"></input>
-      <span class="text-sm">
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Mot de passe</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+      </div>
+
       {error && <p>{error}</p>}
-      </span>
-    </label>
-  </div>
-  <div class="md:flex md:items-center">
-    <div class="md:w-1/3"></div>
-    <div class="md:w-2/3">
-      <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-        Sign Up
-      </button>
-    </div>
-  </div>
-</form>
-</section>
 
-  )
+      <button type="submit">S'inscrire</button>
+    </form>
+  );
 }
 
 export default Formulaires;
