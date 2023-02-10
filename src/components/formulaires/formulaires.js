8cb31753b868826b54360b38ab258b6b4ef6143f
sebastian Reyes <sebreyes222gmail.com>
2023-02-10 12:10:41 +0100

    ajut valid

--- src/components/formulaires/formulaires.js
+++ src/components/formulaires/formulaires.js
@@ -1,26 +1,28 @@
 import React from 'react'
-// import { useState } from 'react'
+import { useState } from 'react'
 
 const Formulaires = () => {
+  const [first, setFirst] = useState("")
+  const [name, setName] = useState("");
+  const[email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const handleSubmit = event => {
+    event.preventDefault();
+    setError('');
 
-    // let [name, setName] = useState("")
-    // let  input  = document.getElementsByTagName('input')[0]
-    // let task = input.value
-    // const Handlesubmit = (e)=>{
-    //   e.preventDefault();
-      
-    //   if (task != input.value) {
-    //       alert('introduisez votre nom')
-    //       return
-    //   }else{ 
-    //     alert(`le nom que vous avez rentrez est :${name}`)
-    //     return
-    //   }
-    // }
+    if (!name || !email || !password || !first) {
+      setError('Tous les champs sont requis');
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setError('L\'email est invalide');
+    } else {
+      // Envoyer les données au serveur
+    }
+  };
   return (
 
 <section className="bg-gray-50 dark:bg-gray-900">
-<form class="w-full max-w-sm">
+<form class="w-full max-w-sm" onSubmit={handleSubmit}>
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
@@ -28,7 +30,7 @@ const Formulaires = () => {
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder='Nom' ></input>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"value={first} onChange={event => setFirst(event.target.value)} placeholder='' ></input>
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
@@ -38,7 +40,7 @@ const Formulaires = () => {
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="email" placeholder="prénom"></input>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" value={name} onChange={event => setName(event.target.value)} placeholder="prénom"></input>
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
@@ -48,7 +50,7 @@ const Formulaires = () => {
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="email"></input>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="email" value={email} onChange={event => setEmail(event.target.value)} placeholder="email"></input>
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
@@ -58,17 +60,17 @@ const Formulaires = () => {
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="*********"></input>
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" value={password} onChange={event => setPassword(event.target.value)}placeholder="*********"></input>
     </div>
   </div>
-
+  {error && <p>{error}</p>}
   
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3"></div>
     <label class="md:w-2/3 block text-gray-500 font-bold">
       <input class="mr-2 leading-tight" type="checkbox"></input>
       <span class="text-sm">
-        Send me your newsletter!
+      {error && <p>{error}</p>}
       </span>
     </label>
   </div>
