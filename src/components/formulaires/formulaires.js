fd5b15bcb581d24dfe462f547848b194f570334c
sebastian Reyes <sebreyes222gmail.com>
2023-02-10 09:55:58 +0100

    ajout form1

--- src/components/formulaires/formulaires.js
+++ src/components/formulaires/formulaires.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 
 const formulaires = () => {
+    let [name, setName] = useState("")
+    const Handlesubmit = (e)=>{
+      <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+    }
   return (
     <div>
-        
+
     </div>
   )
 }
